c2e4c5129642a1acc3410c8898841e7423109c2b
Sean Waters <skwaters@uark.edu>
2019-01-26 12:17:06 -0600

    Login feature working locally

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 module.exports = {
-
     'googleAuth' : {
         'clientID'         : '865538622102-s2h3194vt7nlk789msp0mgi9g5gpgmoa.apps.googleusercontent.com',
         'clientSecret'     : 'Lr8F8475ZdC-DO98yBstoCUU',
