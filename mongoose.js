797d82aa3d51e54ec2b2da41f1ed911e428fa859
Sean Waters <skwaters@uark.edu>
2019-01-25 20:39:52 -0600

    Testing

--- mongoose.js
+++ mongoose.js
@@ -6,6 +6,33 @@ var Schema = mongoose.Schema;
 module.exports = function () {
     let uri = 'mongodb://heroku_szwx2b9z:8hgof8e0ch1md65gktvu0hv2m3@ds129966.mlab.com:29966/heroku_szwx2b9z';
     //let uri = 'mongodb://localhost:27017/social-auth-example';
+
+    var MongoClient = require('mongodb').MongoClient, assert = require('assert');
+    // Use connect method to connect to the Server
+    MongoClient.connect(uri, function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+
+        var UserSchema = new Schema({
+            email: {
+                type: String, required: true,
+                trim: true, unique: true,
+                match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+            },
+            googleProvider: {
+                type: {
+                    id: String,
+                    token: String
+                },
+                select: false
+            }
+        });
+
+        const User = mongoose.model('User', UserSchema);
+
+        db.close();
+    });
+
     var db = mongoose.connect(uri, {useNewUrlParser: true});//.then(() =>  console.log('connection successful')).catch((err) => console.error(err));
     mongoose.set('useCreateIndex', true);
 
