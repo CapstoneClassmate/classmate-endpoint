4899ae70213b50ea309be79f7af714321fac1490
malec <alec@ahlbrandt.net>
2019-02-05 17:10:26 -0600

    actually save changes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 let googleAuthConfig = {
     clientID: process.env.Google_ClientID || '865538622102-s2h3194vt7nlk789msp0mgi9g5gpgmoa.apps.googleusercontent.com',
-    clientSecret: process.env.Google_ClientSecret || 'Lr8F8475ZdC-DO98yBstoCUU'
+    clientSecret: process.env.Google_Client_Secret || 'Lr8F8475ZdC-DO98yBstoCUU'
 };
 
 let getMongoURI = () => {
